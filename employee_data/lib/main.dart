0ec740b0fee47eff06bea2cc674600c7f2a8455e
flutterrushikesh <flutterrushikesh@gmail.com>
2024-03-10 00:08:10 +0530

    database created

--- employee_data/lib/main.dart
+++ employee_data/lib/main.dart
@@ -1,18 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
-void main() {
-  runApp(const MainApp());
+dynamic database;
+
+class EmployeeData {
+  int empId;
+  double sal;
+  String name;
+
+  EmployeeData({required this.empId, required this.sal, required this.name});
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+void main() async {
+  runApp(const MyApp());
 
+  database = openDatabase(
+    join(await getDatabasesPath(), "EmployeeDB.db"),
+    version: 1,
+    onCreate: (db, version) {
+      db.execute('''CREATE TABLE Employee (
+      empId INT PRIMARY KEY,
+      eName TEXT,
+      sal REAL 
+      )''');
+    },
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text("Hello world"),
         ),
       ),
     );
